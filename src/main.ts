3ad20628752a2711a96ed8beaabf351bd283280d
Andreas Retzius <andreasretzius@gmail.com>
2024-01-10 09:16:26 +0100

    spread strings and objects

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,13 @@ for (let i = 0; i < catArray.length; i++) {
 //Less code than looping and pushing.
 const catArraySpread = [...catArray];
 console.log(catArraySpread);
+
+const catString = "SUPERCUTECATS";
+
+const catStringSpread = [...catString];
+
+console.log(catStringSpread);
+
+const cat = {name: "meow", breed:"Barn cat"};
+const copycat = {...cat};
+console.log(copycat);
